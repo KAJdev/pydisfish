aca538cb3e774b4e92469b4f0dd7c8f724088e16
kaj <40004347+KAJdev@users.noreply.github.com>
2021-11-04 14:49:30 -0800

    lowercase domains

--- pydisfish/utils.py
+++ pydisfish/utils.py
@@ -2,4 +2,4 @@
 
 def get_domain(url: str) -> str:
     """Return the domain from the url"""
-    return url.split('?')[0].split('#')[0].replace('http://', '').replace('https://', '').split('/')[0]
+    return url.split('?')[0].split('#')[0].replace('http://', '').replace('https://', '').split('/')[0].lower()
